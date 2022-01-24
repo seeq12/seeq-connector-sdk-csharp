f3afbc265131b3100e58d16f47c2b771a749d275
Marius Oancea <marius.oancea@seeq.com>
2022-01-24 17:14:07 +0200

    Remove libraries and change the csproj files

---
+++ clean.bat
@@ -0,0 +1,4 @@
+echo y|rmdir /s MyCompany.Seeq.Link.Connector.MyConnector\bin 
+echo y|rmdir /s MyCompany.Seeq.Link.Connector.MyConnector\obj
+echo y|rmdir /s c:\seeq-connector-sdk\csharp-new\Seeq.Link.SDK.Debugging.Agent\bin 
+echo y|rmdir /s c:\seeq-connector-sdk\csharp-new\Seeq.Link.SDK.Debugging.Agent\obj
