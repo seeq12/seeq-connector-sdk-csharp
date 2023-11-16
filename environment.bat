2201f2a8ee41e56f9ca492c7b82fbf7b10f147b1
Winner-Timothy Bolorunduro <winner.bolorunduro@seeq.com>
2023-11-16 21:03:19 +0100

    CRAB-37569: add in console testing support

--- environment.bat
+++ environment.bat
@@ -13,5 +13,6 @@ echo Your connector name is currently "%SEEQ_CONNECTOR_NAME%"
 echo.
 echo First, execute "build" to compile the connector.
 echo Then execute "ide" to launch Visual Studio for development and debugging.
+echo You could run "test" to execute tests you have included.
 echo Finally, execute "package" to build a deployable connector package.
 echo.
