2afb0b5e5d07d75f59e8afeed433d5f398e3757c
Bolorunduro Winner-Timothy <bolorundurowb@gmail.com>
2024-08-23 17:40:07 +0100

    CRAB-44040: add standard test scaffolds

---
+++ MyCompany.Seeq.Link.Connector.MyConnector.Test/MyConnectorConditionStandardTest.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using Seeq.Link.SDK;
+using Seeq.Link.SDK.TestFramework;
+
+namespace MyCompany.Seeq.Link.Connector.MyConnectorTest {
+
+    [TestFixture]
+    public class MyConnectorConditionStandardTest : ConditionPullConnectionTestSuite<MyConnection, MyConnector, MyConnectionConfigV1, MyConnectorConfigV1> {
+        public override MyConnection Connection => throw new System.NotImplementedException();
+
+        public override MyConnector Connector => throw new System.NotImplementedException();
+
+        public override void BaseConnectionOneTimeSetUp() {
+            throw new System.NotImplementedException();
+        }
+
+        public override void ConditionPullConnectionOneTimeSetUp() {
+        }
+
+        public override List<ConfigObject> ConnectorConfigVersions() => new List<ConfigObject>
+            {
+                new MyConnectorConfigV1()
+            };
+
+        public override string DataIdForTest(string testName) {
+            throw new System.NotImplementedException();
+        }
+
+        public override List<IgnoredTest> IgnoredTests() => new List<IgnoredTest>();
+
+        public override void IndexingConnectionOneTimeSetUp() {
+        }
+
+        public override void PullConnectionOneTimeSetUp() {
+        }
+    }
+}
---
+++ MyCompany.Seeq.Link.Connector.MyConnector.Test/MyConnectorSignalStandardTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using Seeq.Link.SDK;
+using Seeq.Link.SDK.TestFramework;
+
+namespace MyCompany.Seeq.Link.Connector.MyConnectorTest {
+
+    [TestFixture]
+    public class MyConnectorSignalStandardTest : SignalPullConnectionTestSuite<MyConnection, MyConnector, MyConnectionConfigV1, MyConnectorConfigV1> {
+        public override MyConnection Connection { get; }
+        public override MyConnector Connector { get; }
+        public override List<IgnoredTest> IgnoredTests() => throw new System.NotImplementedException();
+        public override void BaseConnectionOneTimeSetUp() { throw new System.NotImplementedException(); }
+        public override List<ConfigObject> ConnectorConfigVersions() => throw new System.NotImplementedException();
+        public override void IndexingConnectionOneTimeSetUp() { throw new System.NotImplementedException(); }
+        public override void PullConnectionOneTimeSetUp() { throw new System.NotImplementedException(); }
+        public override string DataIdForTest(string testName) => throw new System.NotImplementedException();
+        public override void SignalPullConnectionOneTimeSetUp() { throw new System.NotImplementedException(); }
+    }
+}
