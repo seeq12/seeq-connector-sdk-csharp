fcdabb86078696090ab0e180705da8a2593438c5
Bolorunduro Winner-Timothy <bolorundurowb@gmail.com>
2024-09-05 13:10:47 +0100

    CRAB-44040: rework the clean script

--- clean.bat
+++ clean.bat
@@ -1,6 +1,6 @@
-echo y|rmdir /s MyCompany.Seeq.Link.Connector.MyConnector\bin 
-echo y|rmdir /s MyCompany.Seeq.Link.Connector.MyConnector\obj
-echo y|rmdir /s Seeq.Link.SDK.Debugging.Agent\bin
-echo y|rmdir /s Seeq.Link.SDK.Debugging.Agent\obj
-echo y|rmdir /s MyCompany.Seeq.Link.Connector.MyConnector.Test\bin 
-echo y|rmdir /s MyCompany.Seeq.Link.Connector.MyConnector.Test\obj
+rmdir /Q /S MyCompany.Seeq.Link.Connector.MyConnector\bin  >nul 2>&1
+rmdir /Q /S MyCompany.Seeq.Link.Connector.MyConnector\obj >nul 2>&1
+rmdir /Q /S Seeq.Link.SDK.Debugging.Agent\bin >nul 2>&1
+rmdir /Q /S Seeq.Link.SDK.Debugging.Agent\obj >nul 2>&1
+rmdir /Q /S MyCompany.Seeq.Link.Connector.MyConnector.Test\bin >nul 2>&1 
+rmdir /Q /S MyCompany.Seeq.Link.Connector.MyConnector.Test\obj >nul 2>&1
