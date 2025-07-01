6e3fd1367e618f009f28b6e439713b61984b980c
Marius Oancea <marius.oancea@seeq.com>
2025-07-01 21:48:39 +0300

    CRAB-46033: Use CapsuleIdProperty in csharp-connector-sdk-example

--- MyCompany.Seeq.Link.Connector.MyConnector/DatasourceSimulator.cs
+++ MyCompany.Seeq.Link.Connector.MyConnector/DatasourceSimulator.cs
@@ -89,7 +89,7 @@ namespace MyCompany.Seeq.Link.Connector {
                 )
                 .Select(index => {
                     DateTime start = new TimeInstant(index * capsulePeriodInNanos).ToDateTimeRoundDownTo100ns();
-                    DateTime end = start + TimeSpan.FromTicks(100);
+                    DateTime end = start + TimeSpan.FromMilliseconds(10000L);
                     return new Alarm.Event(start, end, Rng.NextDouble());
                 })
                 .Take(limit);
